8c5404195cca0c590e4dc4e1191fcee9471c9f17
Shagun0409 <shagun.sg2003@gmail.com>
2025-11-25 01:15:39 +0530

    cra

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    
+      <div>
+         <h1 className='text-5xl'>Vite + React</h1>
+        
+      </div>
+     
+
+  )
+}
+
+export default App
